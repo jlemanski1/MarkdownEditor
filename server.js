5e1a34b76c13f14158f3ee67c00e4d8b599b2998
Jonathan Lemanski <jlemanski1@gmail.com>
2020-05-21 13:12:48 -0600

    Setup express app and made test route

---
+++ server.js
@@ -0,0 +1,22 @@
+/*
+    SERVERJS
+    Application entry point
+*/
+
+
+var express = require('express');
+var app = express();
+
+app.set('view engine', 'ejs');
+
+// Set app to use public folder where assets are stored
+app.use(express.static(__dirname + '/public'));
+
+// App Routes
+app.get('/', function(req, res) {
+    res.render('mkdViewer');
+});
+
+// Listen on port 8000 for localhost
+var port = 8000;
+app.listen(port);
