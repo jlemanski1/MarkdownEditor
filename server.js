77e80c62ec864308e949563d3ade0404231a4741
Jonathan Lemanski <jlemanski1@gmail.com>
2020-05-27 09:26:10 -0600

    fixed placeholder markdown, got redis server working, and wrote readme using project

--- server.js
+++ server.js
@@ -26,12 +26,15 @@ app.get('/(:id)', function(req, res) {
 var redisClient;
 console.log(process.env.REDISTOGO_URL);
 if (process.env.REDISTOGO_URL) {
-  var rtg   = require("url").parse(process.env.REDISTOGO_URL);
+  var rtg = require("url").parse(process.env.REDISTOGO_URL);
   redisClient = require("redis").createClient(rtg.port, rtg.hostname);
   redisClient.auth(rtg.auth.split(":")[1]);
 } else {
   redisClient = require("redis").createClient();
 }
+redisClient.on('error', function(err) {
+    console.log(err);
+});
 
 // ShareJS options
 var options = {
